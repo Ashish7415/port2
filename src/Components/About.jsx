150b90b422441a9a2e41739d2a4c78ad3c829ce1
ashishpatel0981@gmail.com <ashishpatel0981@gmail.com>
2024-11-14 12:36:26 +0530

    yes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -59,7 +59,7 @@ const About = () => {
                   className="text-2xl font-bold text-transparent bg-clip-text 
                 bg-gradient-to-r from-green-400 to-blue-500"
                 >
-                  +
+                  1+
                 </h3>
                 <p>Years Experience</p>
               </div>
